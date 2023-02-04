d15d0eed9fcbb8add4aabc7bc302d7badd48342a
AsumVictor <Iamasum369@gmail.com>
2023-02-04 21:28:11 +0000

    rellax initiated

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,9 @@
 const pointer_circle = document.querySelector('.circle')
 
 document.addEventListener('mousemove',(e)=>{
-    console.log(e);
     pointer_circle.style.top = (e.clientY-26)+"px"
     pointer_circle.style.left = (e.clientX-26)+'px'
 })
+
+// Introducing rellax
+
